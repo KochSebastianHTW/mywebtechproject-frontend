22a96a0dbb20ac2aba3fd4f77f5643424c67bcd2
paulm <Sebastian.Koch@Student.HTW-Berlin.de>
2022-11-08 16:06:46 +0100

    dynamically view to show cards

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <button type="button" class="btn btn-primary">Primary</button>
+    <div id="liveAlertPlaceholder"></div>
+    <button type="button" class="btn btn-primary">Drück mich!</button>
   </div>
 </template>
 
--- src/views/Cards.vue
+++ src/views/Cards.vue
@@ -1,11 +1,46 @@
 <template>
   <h1>Welcome to Cards</h1>
+  <div class="card-group" v-for="card in cards" :key="card.id">
+    <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="card-header">{{ card.name }}</h5>
+        <small class="text-muted">3 days ago</small>
+      </div>
+      <div class="d-flex w-100 justify-content-between">
+        <p class="card-text">{{ card.description }}</p>
+        <small class="text-end">{{ card.label_ids }}</small>
+      </div>
+      <p class="card-footer">{{ card.dueDate }}</p>
+    </a>
+  </div>
 </template>
 
 <script>
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Cards'
+  name: 'Cards',
+  data () {
+    return {
+      cards: [
+        {
+          id: 1,
+          name: 'M2',
+          dueDate: '14.11.2022T17:00',
+          description: 'blabla',
+          register: 'DONE'.toLocaleLowerCase(),
+          label_ids: null
+        },
+        {
+          id: 2,
+          name: 'M3',
+          dueDate: '05.12.2022T17:00',
+          description: 'bliblablup',
+          register: 'IN PROGRESS'.toLocaleLowerCase(),
+          label_ids: 4
+        }
+      ]
+    }
+  }
 }
 </script>
 
