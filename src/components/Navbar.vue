4bc8c75ba790f65f75fc258e5b873bc243277fae
paulm <Sebastian.Koch@Student.HTW-Berlin.de>
2022-11-08 14:33:19 +0100

    configurations for Heroku deploy

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,14 @@
 <template>
   <nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">U Know What ToDo</a>
+      <router-link class="navbar-brand" to="/">U Know What ToDo</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <div class="navbar-nav">
-          <router-link class="nav-link" to="/">Home</router-link>
           <router-link class="nav-link" to="/about">About</router-link>
-          <router-link class="nav-link" to="/cards">Your Board</router-link>
+          <router-link class="nav-link" to="/cards">Board</router-link>
         </div>
       </div>
     </div>
